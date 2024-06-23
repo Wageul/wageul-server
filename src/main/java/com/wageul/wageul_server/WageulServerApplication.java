3cd2dea8dfca3c5bc245bb8667312f225389ba84
ywonchae1 <ywonchae62@gmail.com>
2024-06-23 21:43:35 +0900

    first commit

---
+++ src/main/java/com/wageul/wageul_server/WageulServerApplication.java
@@ -0,0 +1,13 @@
+package com.wageul.wageul_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WageulServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WageulServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wageul/wageul_server/WageulServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wageul.wageul_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WageulServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

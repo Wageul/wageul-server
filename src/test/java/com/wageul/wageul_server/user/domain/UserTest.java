d08bd8449169a9a75b1690d49efe1788ee1d5645
ywonchae1 <ywonchae62@gmail.com>
2024-06-25 14:40:52 +0900

    test: 회원 정보 수정 테스트코드 작성 (#8)

---
+++ src/test/java/com/wageul/wageul_server/user/domain/UserTest.java
@@ -0,0 +1,35 @@
+package com.wageul.wageul_server.user.domain;
+
+import com.wageul.wageul_server.user.dto.UserUpdate;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @Test
+    void 상세정보_업데이트() {
+        // given
+        User user = User.builder()
+                .id(1)
+                .email("ywonchae62@gmail.com")
+                .profileImg("abc.png")
+                .name("wonchae")
+                .nationality("Korea")
+                .introduce("")
+                .build();
+        UserUpdate userUpdate = UserUpdate.builder()
+                .profileImg("haha.png")
+                .nationality("Korea")
+                .introduce("hello world~")
+                .build();
+        // when
+        user = user.update(userUpdate);
+
+        // then
+        Assertions.assertThat(user.getProfileImg()).isEqualTo("haha.png");
+        Assertions.assertThat(user.getNationality()).isEqualTo("Korea");
+        Assertions.assertThat(user.getIntroduce()).isEqualTo("hello world~");
+    }
+}
---
+++ src/test/java/com/wageul/wageul_server/user/service/UserServiceTest.java
@@ -0,0 +1,39 @@
+package com.wageul.wageul_server.user.service;
+
+import com.wageul.wageul_server.common.JwtTokenGenerator;
+import com.wageul.wageul_server.user.domain.User;
+import com.wageul.wageul_server.user.dto.UserUpdate;
+import com.wageul.wageul_server.user.mock.FakeUserRepository;
+import com.wageul.wageul_server.user.service.port.UserRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class UserServiceTest {
+
+    @Test
+    void 회원정보_수정() {
+        // given
+        UserService userService = new UserService(new FakeUserRepository(), new JwtTokenGenerator());
+        User user = User.builder()
+                .id(1)
+                .email("ywonchae62@gmail.com")
+                .profileImg("abc.png")
+                .name("wonchae")
+                .nationality("Korea")
+                .introduce("")
+                .build();
+        UserUpdate userUpdate = UserUpdate.builder()
+                .profileImg("haha.png")
+                .nationality("Korea")
+                .introduce("hello world~")
+                .build();
+
+        // when
+        User updatedUser = userService.update(user, userUpdate);
+
+        // then
+        Assertions.assertThat(updatedUser.getProfileImg()).isEqualTo("haha.png");
+        Assertions.assertThat(updatedUser.getNationality()).isEqualTo("Korea");
+        Assertions.assertThat(updatedUser.getIntroduce()).isEqualTo("hello world~");
+    }
+}

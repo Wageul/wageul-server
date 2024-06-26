31f468bbee0ae77cc2255427985d8faf4288e493
ywonchae1 <ywonchae62@gmail.com>
2024-06-26 23:59:37 +0900

    feat: 로그인된 사용자 비교 추가 (#16)

--- src/main/java/com/wageul/wageul_server/user/service/UserService.java
+++ src/main/java/com/wageul/wageul_server/user/service/UserService.java
@@ -1,37 +1,30 @@
 package com.wageul.wageul_server.user.service;
 
-import com.wageul.wageul_server.common.JwtTokenGenerator;
-import com.wageul.wageul_server.common.response.GoogleAccountProfileResponse;
+import com.wageul.wageul_server.oauth2.AuthorizationUtil;
 import com.wageul.wageul_server.user.domain.User;
 import com.wageul.wageul_server.user.dto.UserUpdate;
 import com.wageul.wageul_server.user.service.port.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
-    private final JwtTokenGenerator jwtTokenGenerator;
+    private final AuthorizationUtil authorizationUtil;
 
-    public User findOrCreate(GoogleAccountProfileResponse profile) {
-        return userRepository.findByEmail(profile.email())
-                .orElseGet(() -> userRepository.save(User.builder()
-                        .email(profile.email())
-                        .name(profile.name())
-                        .profileImg(profile.picture())
-                        .build()));
-    }
-
-    public User getById(long id, String token) {
-        long userId = -1;
-        if(token != null) jwtTokenGenerator.extractUserId(token);
-        if(userId == id)
+    public User getById(long id) {
+        long userId = authorizationUtil.getLoginUserId();
+        if(userId == id) {
             return userRepository.findById(id).orElse(null);
-        else
+        } else {
             return null;
+        }
     }
 
     @Transactional

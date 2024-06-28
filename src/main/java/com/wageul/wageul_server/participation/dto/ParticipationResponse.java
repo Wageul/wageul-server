74391dd5d3806e4353a19f521862c26f281ddeee
ywonchae1 <ywonchae62@gmail.com>
2024-06-28 09:51:55 +0900

    mod: 체험 참가 응답 형식 수정 (#28)

---
+++ src/main/java/com/wageul/wageul_server/participation/dto/ParticipationResponse.java
@@ -0,0 +1,35 @@
+package com.wageul.wageul_server.participation.dto;
+
+import com.wageul.wageul_server.experience.domain.Experience;
+import com.wageul.wageul_server.experience.dto.ExperienceNoWriterResponse;
+import com.wageul.wageul_server.participation.domain.Participation;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class ParticipationResponse {
+	private final long id;
+	private final long userId;
+	private final ExperienceNoWriterResponse experience;
+
+	@Builder
+	public ParticipationResponse(Participation participation) {
+		this.id = participation.getId();
+		this.userId = participation.getUser().getId();
+		Experience experience = participation.getExperience();
+		this.experience = ExperienceNoWriterResponse.builder()
+			.id(experience.getId())
+			.title(experience.getTitle())
+			.location(experience.getLocation())
+			.datetime(experience.getDatetime())
+			.content(experience.getContent())
+			.duration(experience.getDuration())
+			.cost(experience.getCost())
+			.contact(experience.getContact())
+			.limitMember(experience.getLimitMember())
+			.writerId(experience.getWriter().getId())
+			.language(experience.getLanguage())
+			.build();
+	}
+}

a16a82290d3c8aeabc37ee8b7d3b9f411f1d2799
ywonchae1 <ywonchae62@gmail.com>
2024-07-04 18:01:23 +0900

    feat: 체험 별 참여자 정보 응답 객체 생성 (#34)

---
+++ src/main/java/com/wageul/wageul_server/participation/dto/ExperienceParticipationResponse.java
@@ -0,0 +1,25 @@
+package com.wageul.wageul_server.participation.dto;
+
+import java.util.List;
+
+import com.wageul.wageul_server.user.domain.User;
+import com.wageul.wageul_server.user.dto.UserSimpleProflieDto;
+
+import lombok.Getter;
+
+@Getter
+public class ExperienceParticipationResponse {
+
+	private final long experienceId;
+	private final List<UserSimpleProflieDto> userSimpleProflieList;
+
+	public ExperienceParticipationResponse(long experienceId, List<User> users) {
+		this.experienceId = experienceId;
+		this.userSimpleProflieList = users.stream().map(user -> {
+			return UserSimpleProflieDto.builder()
+				.profileImg(user.getProfileImg())
+				.name(user.getName())
+				.build();
+		}).toList();
+	}
+}

6ffd3089fbe1e46524c37cbec039c00999c1f56c
ywonchae1 <ywonchae62@gmail.com>
2024-07-04 17:50:10 +0900

    fix: Service 로직에서 Exception이 발생하면 무조건 구글 로그인 html이 반환되는 오류 수정 (#34)

--- src/main/java/com/wageul/wageul_server/config/SecurityConfig.java
+++ src/main/java/com/wageul/wageul_server/config/SecurityConfig.java
@@ -5,12 +5,14 @@ import java.util.Collections;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.client.web.OAuth2LoginAuthenticationFilter;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
@@ -73,7 +75,7 @@ public class SecurityConfig {
 
 		//JWTFilter 추가
 		http
-			.addFilterBefore(new JwtFilter(jwtUtil), OAuth2LoginAuthenticationFilter.class);
+			.addFilterAfter(new JwtFilter(jwtUtil), OAuth2LoginAuthenticationFilter.class);
 
 		//oauth2
 		http
@@ -92,6 +94,14 @@ public class SecurityConfig {
 		http
 			.sessionManagement((session) -> session
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+		// /api로 시작하는 url인 경우 401 상태코드를 반환하도록 예외 처리
+		http
+			.exceptionHandling(exceptionHandling -> exceptionHandling
+				.defaultAuthenticationEntryPointFor(
+					new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED),
+					new AntPathRequestMatcher("/api/**")
+				));
 
 		return http.build();
 	}
